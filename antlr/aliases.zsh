02c61cef1ac0df0ebae8588c5c50a28bcfffce1b
Brian Stewart <brian@thecodesmith.com>
2015-04-04 20:57:55 -0500

    Added antlr, groovyserv, lazybones, zshrc

---
+++ antlr/aliases.zsh
@@ -0,0 +1,2 @@
+alias antlr='java -Xmx500M -cp "/usr/local/lib/antlr-4.5-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
+alias grun='java org.antlr.v4.runtime.misc.TestRig'
---
+++ groovyserv/aliases.zsh
@@ -0,0 +1,1 @@
+alias groovy='groovyclient'
