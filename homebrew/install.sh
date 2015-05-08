45eaec243a5924dcf9fbddb50b58d19bb1cbf651
Brian Stewart <brian@thecodesmith.com>
2015-05-07 19:39:39 -0500

    Added common utils to homebrew install, configured zsh to use vi-mode

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,6 +13,27 @@ then
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark git vim hub
+brew install grc 
+brew install coreutils 
+brew install spark 
+brew install git 
+brew install vim 
+brew install hub
+brew install findutils --default-names
+brew install gnu-sed --default-names
+brew install gnu-tar --default-names
+brew install gnu-which --default-names
+brew install gnutls --default-names
+brew install grep --default-names
+brew install coreutils 
+brew install binutils 
+brew install diffutils 
+brew install gzip 
+brew install watch 
+brew install tmux 
+brew install wget 
+brew install nmap 
+brew install gpg 
+brew install htop 
 
 exit 0
