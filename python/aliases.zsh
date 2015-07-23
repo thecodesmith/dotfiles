dfa6bdc1a1655e3a6235a8bdb5794e36b1756770
Brian Stewart <brian@thecodesmith.com>
2015-07-23 08:28:24 -0500

    Added django aliases

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -2,4 +2,7 @@ alias python2=`which python`
 alias python=`which python3`
 
 alias venv='source ./venv/bin/activate'
-alias dj='./manage'
+alias dj='./manage.py'
+
+alias ut='./manage.py test'
+alias ft='./manage.py test functional_tests'
