094f36086fae99123982ab3e7f3e684fd595c5e9
Brian Stewart <brian@thecodesmith.com>
2015-08-14 21:45:58 -0500

    Added OS variable, fixed `ls` cmd on OS X

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,13 @@
-alias ls='ls --color=auto'
-alias ll='ls -al --color=auto'
+if [[ "$OS" == 'OSX' ]]
+then
+  alias ls='ls -G'
+  alias ll='ls -alG'
+elif [[ "$OS" == 'LINUX' ]]
+then
+  alias ls='ls --color=auto'
+  alias ll='ls -al --color=auto'
+fi
+
 alias reload!='. ~/.zshrc'
 
 alias ..='cd ..'
