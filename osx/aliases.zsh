b46ddc3e2e5a6ec7e08487361b8e7bfc5ae1f73c
Brian Stewart <brian.stewart@jamfsoftware.com>
2016-08-23 13:15:43 -0500

    Added screen sharing on/off command

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,5 +1,21 @@
-if [[ $OS == 'OSX' ]]
+#!/usr/bin/env bash
+
+if [[ $OS != 'OSX' ]]
 then
-  alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-  alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+    exit
 fi
+
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+ss() {
+    if [[ "$1" == "on" ]]
+    then
+        sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool false
+        sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
+    elif [[ "$1" == "off" ]]
+    then
+        sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool true
+        sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
+    fi
+}
