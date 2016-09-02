724a57b2608c5784dc550b045f7c911fe2ee8dd0
Brian Stewart <brian.stewart@jamfsoftware.com>
2016-09-02 11:23:06 -0500

    Added ip alias

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -8,13 +8,17 @@ fi
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
+ip() {
+    ifconfig | grep inet | cut -f2 -d ' ' | grep -v ':' | grep -v '^127.0.0.1'
+}
+
 ss() {
     if [[ "$1" == "on" ]]
     then
         sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool false
         sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
 
-        ifconfig | grep inet | cut -f4 -d ' ' | grep -v ':' | grep -v '^127.0.0.1'
+        ip
     elif [[ "$1" == "off" ]]
     then
         sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool true
