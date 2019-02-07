0c00cb299df890973190ff6b0c465192fb5d1602
Brian Stewart <brian.stewart@jamf.com>
2019-02-07 16:58:08 -0600

    Add Jenkins configuration

---
+++ jenkins/aliases.zsh
@@ -0,0 +1,1 @@
+alias jenkins='java -jar ~/.jamf/bin/jenkins-cli.jar -noKeyAuth -s $JENKINS_URL'
--- jenkins/env.zsh
+++ jenkins/env.zsh
@@ -1,1 +1,4 @@
+source ~/.jamf/jenkins.sh
+
 export JENKINS_URL=https://jenkins.kube.jamf.build
+export JENKINS_USER_ID=brian.stewart
