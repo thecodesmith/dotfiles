fadc77d84123442583ac132b99ced79b14d7961b
Brian Stewart <brian.stewart@jamf.com>
2019-05-29 09:14:25 -0500

    Add kube config

---
+++ kube/env.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+[ -f ~/.kubectl-aliases/.kubectl_aliases ] && source ~/.kubectl-aliases/.kubectl_aliases
---
+++ kube/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+brew install kubectl
+brew install kubectx
+
+git clone git@github.com:ahmetb/kubectl-aliases.git ~/.kubectl_aliases
