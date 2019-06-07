1314b4f53968000f79829594f181707daa04de06
Brian Stewart <brian.stewart@jamf.com>
2019-06-07 10:09:07 -0500

    Start ssh agent

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/thecodesmith/dot-ssh ~/.ssh
-git clone github:thecodesmith/dotfiles ~/.dotfiles
+git clone https://github.com/thecodesmith/dotfiles ~/.dotfiles
 
 cd ~/.dotfiles
+source ssh/ssh-agent.zsh
 ./script/bootstrap
 ./script/install
