95d657e7bbdd69747f83ca6078beb7989668c004
Brian Stewart <brian.stewart@jamf.com>
2019-12-17 12:45:20 -0600

    Update sdkman init

--- sdkman/path.zsh
+++ sdkman/path.zsh
@@ -1,3 +1,3 @@
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="/Users/brian/.sdkman"
-[[ -s "/Users/brian/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/brian/.sdkman/bin/sdkman-init.sh"
+# export SDKMAN_DIR="/Users/brian/.sdkman"
+# [[ -s "/Users/brian/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/brian/.sdkman/bin/sdkman-init.sh"
