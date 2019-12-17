cd1c1bfc3dfb38f4aa597465cdd886cf52e3b272
Brian Stewart <brian.stewart@jamf.com>
2019-12-17 12:44:04 -0600

    Add kube aliases

--- kube/aliases.zsh
+++ kube/aliases.zsh
@@ -1,3 +1,12 @@
 alias k=kubectl
 alias kx=kubectx
 alias kns=kubens
+alias ka='kubectl apply -f'
+alias kg='kubectl get'
+# alias kgp='kubectl get po'
+alias kd='kubectl describe'
+alias klint='kubeval --strict'
+
+function kgp() {
+    kubectl get po $@ | kubectl-colorized
+}
