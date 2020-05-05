7a4dd0b350a7d25319265c5fb82a288d4d735fd5
Brian Stewart <brian.stewart@jamf.com>
2020-05-05 16:14:46 -0500

    Update jenkins

--- jenkins/env.zsh
+++ jenkins/env.zsh
@@ -1,6 +1,6 @@
 source ~/.jamf/jenkins.sh
 
-export JENKINS_URL=https://jenkins.kube.jamf.build
+export JENKINS_URL=https://jenkins.jamf.build
 export JENKINS_USER_ID=brian.stewart
 # export JENKINS_URL=http://localhost:8080
 # export JENKINS_USER_ID=admin
