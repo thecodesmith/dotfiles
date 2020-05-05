ae7f95f99632c4fdaa92abd4070aae704caae6cc
Brian Stewart <brian.stewart@jamf.com>
2020-05-05 16:14:28 -0500

    Update go to use modules

--- go/path.zsh
+++ go/path.zsh
@@ -1,4 +1,6 @@
+export GO111MODULE=on
 export GOPATH=$HOME/Projects/go
 export GOROOT=/usr/local/opt/go/libexec
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$GOROOT/bin:$PATH"
+export PATH="${PATH}:${HOME}/.krew/bin"
