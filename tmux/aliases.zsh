bbaa2c1281dba1ceaf6bd150590d2dade74ce5dd
Brian Stewart <brian.stewart@jamf.com>
2020-05-05 16:12:51 -0500

    Tmux updates

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,1 +1,12 @@
 alias tks='tmux kill-session'
+alias tclear='_tmux_clear_all_panes'
+
+_tmux_send_keys_all_panes() {                                                                                  #{{{1
+    for _pane in $(tmux list-panes -F '#{pane_index} #{pane_current_command}' | grep zsh | awk '{ print $1 }'); do
+        tmux send-keys -t ${_pane} "$@"
+    done
+}
+
+_tmux_clear_all_panes() {
+    _tmux_send_keys_all_panes clear Enter
+}
