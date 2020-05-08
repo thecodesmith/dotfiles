45dd6afe431079618c4e5c92581c31669003b24c
Brian Stewart <brian.stewart@jamf.com>
2020-05-08 11:13:25 -0500

    Move jamf config to its own repo

---
+++ jamf/env.zsh
@@ -0,0 +1,3 @@
+if [ -f ~/.jamf/env.zsh ]; then
+    source ~/.jamf/env.zsh
+fi
--- jamf/path.zsh
+++
@@ -1,3 +0,0 @@
-# export PATH=/Library/Java/JavaVirtualMachines/jdk1.8.0_211.jdk/Contents/Home/bin:~/.jamf/bin:$PATH
-export PATH=~/.jamf/bin:$PATH
-alias jpst='cd ~/Projects/jamf/jamf-pro-server-tools'
