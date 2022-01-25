4a7fcb36f2242a99ddd2fefb942f7dd22d9641fa
Brian Stewart <brian.stewart@jamf.com>
2022-01-25 14:20:39 -0600

    Update gradle aliases

--- gradle/aliases.zsh
+++ gradle/aliases.zsh
@@ -1,9 +1,18 @@
 # alias g=gradle
 
 function g() {
+    unset JENKINS_URL
     if [ -f gradlew ]; then
         ./gradlew $@
     else
         gradle $@
     fi
+}
+
+function ginit-enable() {
+    mv ~/.gradle/init.d.bak ~/.gradle/init.d
+}
+
+function ginit-disable() {
+    mv ~/.gradle/init.d ~/.gradle/init.d.bak
 }
