9f8abf8b7a4d6ac5d88366ae779bad895d6a5350
Brian Stewart <brian.stewart@jamf.com>
2022-02-06 17:05:21 -0600

    Update go

--- go/install.sh
+++ go/install.sh
@@ -1,12 +1,1 @@
-GOPATH=$HOME/code/go
-GOROOT=/usr/local/opt/go/libexec
-
-mkdir -p $GOPATH
-mkdir -p $GOPATH/src/github.com/thecodesmith
-
-export GOPATH=$GOPATH
-export GOROOT=$GOROOT
-export PATH=$GOPATH/bin:$PATH
-export PATH=$GOROOT/bin:$PATH
-
 brew install golang
--- go/path.zsh
+++ go/path.zsh
@@ -1,6 +1,8 @@
 export GO111MODULE=on
-export GOPATH=$HOME/code/go
+export GOPATH=$PROJECTS/go
 export GOROOT=/usr/local/opt/go/libexec
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$GOROOT/bin:$PATH"
-export PATH="${PATH}:${HOME}/.krew/bin"
+
+mkdir -p $GOPATH
+mkdir -p $GOPATH/src/github.com/thecodesmith
