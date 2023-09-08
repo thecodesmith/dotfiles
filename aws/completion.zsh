28f4c7989c90b560f179068c6620f289c9ba1819
Brian Stewart <brian.stewart@jamf.com>
2023-09-08 12:35:08 -0500

    Fix aws completion

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -1,5 +1,10 @@
 completion="$(brew --prefix)/share/zsh/site-functions/_aws"
+completer="$(brew --prefix)/share/zsh/site-functions/aws_zsh_completer.sh"
 
 if test -f "$completion"; then
     source "$completion"
+fi
+
+if test -f "$completer"; then
+    source "$completer"
 fi
