c6648fbb34633b65046d1f70e4b987d2b2514ac7
Brian Stewart <brian.stewart@jamf.com>
2024-12-20 14:00:45 -0600

    Add gpg setup instructions

--- gpg/install.sh
+++ gpg/install.sh
@@ -1,1 +1,11 @@
 brew install gpg2 gnupg pinentry-mac
+
+echo 'gpg has been installed'
+echo 'set up existing gpg keys using these steps:'
+echo '  1. export keys from original system'
+echo '     gpg --list-secret-keys'
+echo '     gpg --export <id> > ~/.gnupg/pub.key'
+echo '     gpg --export-secret-key <id> > ~/.gnupg/pri.key'
+echo '  2. copy keys and import on new system'
+echo '     gpg --import pub.key'
+echo '     gpg --import pri.key'
