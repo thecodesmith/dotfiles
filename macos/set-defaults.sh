89ca2c2277585660cacf6963f7f84f65e37eaec1
Brian Stewart <brian.stewart@jamf.com>
2026-02-25 13:01:28 -0600

    Update key repeat delay

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -41,7 +41,7 @@ defaults write /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdv
 defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist redesigned_text_cursor -dict-add Enabled -bool NO
 
 # Speed up key repeat
-defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
 
 # Hide the Dock
