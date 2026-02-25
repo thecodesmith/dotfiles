a940bc387c75f2d9316d2e2cdbe98603f8ca0307
Brian Stewart <brian.stewart@jamf.com>
2026-02-25 13:01:45 -0600

    Update homebrew install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -20,40 +20,44 @@ brew install --cask \
     jetbrains-toolbox
 
 # Install homebrew packages
-brew install bash-completion \
-             bat \
-             python \
-             the_silver_searcher \
-             tree \
-             fzf \
-             gh \
-             git \
-             gpg \
-             grc \
-             htop \
-             nmap \
-             terminal-notifier \
-             tmux \
-             vim \
-             jq \
-             yq \
-             font-hack-nerd-font
+brew install \
+    bash-completion \
+    bat \
+    font-hack-nerd-font \
+    fzf \
+    gh \
+    git \
+    gpg \
+    grc \
+    htop \
+    jq \
+    nmap \
+    pv \
+    python \
+    ripgrep \
+    terminal-notifier \
+    the_silver_searcher \
+    tmux \
+    tree \
+    vim \
+    yq
 
 $(brew --prefix)/opt/fzf/install --all
 
 # UNIX tools
-brew install findutils \
-             gawk \
-             gnu-sed \
-             gnu-tar \
-             gnu-which \
-             gnutls  \
-             grep \
-             coreutils \
-             binutils \
-             diffutils \
-             gzip \
-             watch \
-             wget
+brew install \
+    binutils \
+    coreutils \
+    diffutils \
+    findutils \
+    gawk \
+    gnu-sed \
+    gnu-tar \
+    gnu-which \
+    gnutls  \
+    grep \
+    gzip \
+    watch \
+    wget
 
 exit 0
