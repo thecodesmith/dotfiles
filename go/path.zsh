41a14ae7c6dc9c3c254c19b56bb1b84c102fc98c
Brian Stewart <brian.stewart@jamf.com>
2026-02-25 12:39:45 -0600

    Update GOROOT command

--- go/path.zsh
+++ go/path.zsh
@@ -1,6 +1,6 @@
 export GO111MODULE=on
 export GOPATH=$PROJECTS/go
-export GOROOT="$(brew --prefix)/opt/go/libexec"
+export GOROOT="$(/opt/homebrew/bin/brew --prefix)/opt/go/libexec"
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$GOROOT/bin:$PATH"
 
